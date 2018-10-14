095730a356394d4813daa172cea14bde362515a4
kristophesankar <kristophe.lukas.sankar@gmail.com>
2018-10-14 16:30:39 -0400

    added jQuery plugin functionality

---
+++ js/jquery.christmas-countdown.js
@@ -0,0 +1,98 @@
+(function($) {
+
+    var TargetDate;
+    var BackColor;
+    var ForeColor;
+    var CountActive;
+    var CountStepper;
+    var LeadingZero;
+    var DisplayFormat;
+    var FinishMessage;
+    var SetTimeOutPeriod;
+
+    function calcage(secs, num1, num2) {
+        s = ((Math.floor(secs / num1)) % num2).toString();
+        if (LeadingZero && s.length < 2)
+            s = "0" + s;
+        return "<b>" + s + "</b>";
+    }
+
+    function CountBack(secs) {
+        if (secs < 0) {
+            document.getElementById("cntdwn").innerHTML = FinishMessage;
+            return;
+        }
+        DisplayStr = DisplayFormat.replace(/%%D%%/g, calcage(secs, 86400, 100000));
+        DisplayStr = DisplayStr.replace(/%%H%%/g, calcage(secs, 3600, 24));
+        DisplayStr = DisplayStr.replace(/%%M%%/g, calcage(secs, 60, 60));
+        DisplayStr = DisplayStr.replace(/%%S%%/g, calcage(secs, 1, 60));
+
+        document.getElementById("cntdwn").innerHTML = DisplayStr;
+        if (CountActive) {
+            setTimeout(function() {
+                CountBack(secs + CountStepper);
+            }, SetTimeOutPeriod);
+        }
+    }
+
+    function putspan(backcolor, forecolor) {
+        document.write("<span id='cntdwn' style='background-color:" + backcolor +
+            "; color:" + forecolor + "'></span>");
+    }
+
+    $.fn.christmasCountdown = function(options) {
+        var dt = new Date();
+        var params = $.extend({
+          TargetDate : "12/25/"+dt.getYear()+" 12:00 AM",
+    			BackColor : "white",
+    			ForeColor : "black",
+    			CountActive : true,
+    			CountStepper : -1,
+    			LeadingZero : true,
+    			DisplayFormat : "%%D%% Days, %%H%% Hours, %%M%% Minutes, %%S%% Seconds.",
+    			FinishMessage : "It is finally here! Merry Christmas!"
+        }, options);
+
+        TargetDate = params.TargetDate;
+        BackColor = params.BackColor;
+        ForeColor = params.ForeColor;
+        CountActive = params.CountActive;
+        CountStepper = params.CountStepper;
+        LeadingZero = params.LeadingZero;
+        DisplayFormat = params.DisplayFormat;
+        FinishMessage = params.FinishMessage;
+
+        if (typeof(BackColor) == "undefined")
+            BackColor = "white";
+        if (typeof(ForeColor) == "undefined")
+            ForeColor = "black";
+        if (typeof(TargetDate) == "undefined")
+            TargetDate = "12/31/2020 5:00 AM";
+        if (typeof(DisplayFormat) == "undefined")
+            DisplayFormat = "%%D%% Days, %%H%% Hours, %%M%% Minutes, %%S%% Seconds.";
+        if (typeof(CountActive) == "undefined")
+            CountActive = true;
+        if (typeof(FinishMessage) == "undefined")
+            FinishMessage = "";
+        if (typeof(CountStepper) != "number")
+            CountStepper = -1;
+        if (typeof(LeadingZero) == "undefined")
+            LeadingZero = true;
+
+
+        CountStepper = Math.ceil(CountStepper);
+        if (CountStepper == 0)
+            CountActive = false;
+        SetTimeOutPeriod = (Math.abs(CountStepper) - 1) * 1000 + 990;
+        // putspan(BackColor, ForeColor);
+        var dthen = new Date(TargetDate);
+        var dnow = new Date();
+        if (CountStepper > 0)
+            ddiff = new Date(dnow - dthen);
+        else
+            ddiff = new Date(dthen - dnow);
+        gsecs = Math.floor(ddiff.valueOf() / 1000);
+        CountBack(gsecs);
+    }
+
+}(jQuery));
